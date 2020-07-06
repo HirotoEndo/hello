35862317da6f64e22cb2c8ced8d7c146f479b8be
HirotoEndo <hirt0317@gmail.com>
2020-07-07 05:31:42 +0900

    asa

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -26,9 +26,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
-  end
+
 
 end
