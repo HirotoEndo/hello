74499969dec7c0fd540e696240c7c84f0a7f8006
HirotoEndo <hirt0317@gmail.com>
2020-07-15 15:57:28 +0900

    Implement basic login

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get sessions_new_url
+    assert_response :success
+  end
+
+  test "should get neww" do
+    get login_path
+    assert_response :success
+  end
+end
